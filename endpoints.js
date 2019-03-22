12e8ab5c6202f7418ec899b3cc88663cf8d4444c
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-22 11:31:00 -0600

    got the posts

--- endpoints.js
+++ endpoints.js
@@ -6,7 +6,7 @@ const action = require('./data/helpers/actionModel.js');
 const router = express.Router();
 
 // Get Requests
-router.get('/projects', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const data = await project.get();
     res.status(200).json(data);
@@ -18,15 +18,51 @@ router.get('/projects', async (req, res) => {
   }
 });
 
-router.get('/projects/:projectId', async (req, res) => {
+router.get('/:projectId', async (req, res) => {
   try {
-    console.log(req.params.projectId);
     const data = await project.get(req.params.projectId);
     res.status(200).json(data);
   } catch (error) {
     console.log(error);
     res.status(500).json({
       message: "Error retrieving the actions."
+    });
+  }
+});
+
+// Post Requests
+router.post('/', async (req, res) => {
+  try {
+    const newProject = await project.insert(req.body);
+    if(newProject.name && newProject.description) {
+      res.status(201).json(res.body);
+    } else {
+      res.status(400).json({
+        message: "New Project must have a name and a description."
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error - could not post. Server at fault."
+    });
+  }
+});
+
+router.post('/:projectId', async (req, res) => {
+  try {
+    const newAction = await action.insert(req.body);
+    if(newAction.porject_id && newAction.description && newAction.notes) {
+      res.status(201).json(res.body);
+    } else {
+      res.status(400).json({
+        message: "New Action must have a valid project ID, description, and notes."
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error from server."
     });
   }
 });
--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/endpoints', endpoints);
+server.use('/api/projects', endpoints);
 
 module.exports = server;
